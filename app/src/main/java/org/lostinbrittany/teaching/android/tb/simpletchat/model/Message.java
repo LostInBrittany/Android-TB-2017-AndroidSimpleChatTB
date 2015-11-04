d59844edcac23b0f919e2835d29fc7ac0e66887a
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2015-11-04 02:15:24 +0100

    Message

--- app/src/main/java/org/lostinbrittany/teaching/android/tb/simpletchat/model/Message.java
+++ app/src/main/java/org/lostinbrittany/teaching/android/tb/simpletchat/model/Message.java
@@ -1,7 +1,40 @@
 package org.lostinbrittany.teaching.android.tb.simpletchat.model;
 
-/**
- * Created by horacio on 04/11/15.
- */
+
 public class Message {
+
+    private String username;
+    private String date;
+    private String message;
+
+
+    public Message(String username, String date, String message) {
+        this.username = username;
+        this.date = date;
+        this.message = message;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
